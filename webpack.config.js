ce5db6c1625cd9ae58474b1055e4e8c396128cb4
yanpolonskiy <35795856+yanpolonskiy@users.noreply.github.com>
2019-01-10 22:20:19 +0300

    starting new portfolio

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,187 @@
-let path = require('path');
+"use strict";
+const path = require("path");
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
-module.exports = (env, options) => {
-    // let production = options.mode === 'production';
-    //  conf.devtool = production ? false : 'eval-sourcemap';
+//const BUILD_MODE = process.env.npm_lifecycle_event;
 
-    let conf = {
-        entry: {
-            app: './src/app/index.js'
+module.exports = (env, argv) => {
+  const NODE_ENV = argv.mode;
+  process.env.NODE_ENV = NODE_ENV;
+  const devMode = NODE_ENV === "development";
+  const BUILD_DATETIME = 0; //moment().format("DD.MM.YYYY HH:mm");
+
+  const APP_TITLE = "polonsky-portfolio";
+
+  const plugins = [];
+
+  if (devMode) {
+    plugins.push(new webpack.WatchIgnorePlugin([/less\.d\.ts$/]));
+  } else {
+    plugins.push(
+      new CleanWebpackPlugin(["public"], {
+        root: __dirname,
+        exclude: ["images"]
+      })
+    );
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static",
+        reportFilename: "../report.html"
+      })
+    );
+  }
+
+  return {
+    entry: {
+      app: "./src/index.tsx"
+    },
+    resolve: {
+      alias: {
+        core: path.resolve(__dirname, "src/core")
+      },
+      extensions: [".mjs", ".js", ".ts", ".tsx"]
+    },
+    output: {
+      path: path.resolve(__dirname, "public"),
+      publicPath: "/",
+      filename:
+        "scripts/" + (devMode ? "[name].js" : "[name].[chunkhash:7].js"),
+      chunkFilename:
+        "scripts/" + (devMode ? "[name].js" : "[name].[chunkhash:7].js")
+    },
+    optimization: {
+      runtimeChunk: {
+        name: "manifest"
+      },
+      minimizer: devMode
+        ? []
+        : [
+            new TerserPlugin({
+              //cache: true,
+              parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+          ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|tsx?)$/,
+          exclude: [path.resolve(__dirname, "node_modules")],
+          use: [
+            "babel-loader"
+            //{ loader: "eslint-loader", options: { quiet: !devMode, fix: false } }
+          ]
         },
-        resolve: {
-            alias: {
-                app: path.resolve(__dirname, 'src/app')
+        {
+          test: /\.(less|css)$/,
+          exclude: [path.resolve(__dirname, "src")],
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: { plugins: () => [autoprefixer()] }
+            },
+            {
+              loader: "less-loader",
+              options: { javascriptEnabled: true, relativeUrls: true }
             }
+          ]
         },
-        output: {
-            filename: '[name].js',
-            path: path.resolve(__dirname, 'public'),
+        {
+          test: /\.(less|css)$/,
+          exclude: [path.resolve(__dirname, "node_modules")],
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+              loader: "typings-for-css-modules-loader",
+              options: {
+                modules: true,
+                localIdentName: "[local]--[hash:base64:5]",
+                camelCase: "dashes",
+                namedExport: "camelCase"
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: { plugins: () => [autoprefixer()] }
+            },
+            {
+              loader: "less-loader",
+              options: { javascriptEnabled: true, relativeUrls: true }
+            }
+          ]
         },
-        devServer: {
-            port: 9090,
-            contentBase: path.resolve(__dirname, 'public'),
-            open: true,
-            overlay: true
+        {
+          test: /\.(png|jpg|svg)$/,
+          loader: "url-loader",
+          options: {
+            limit: 8192,
+            name: "img/[name].[ext]"
+          }
         },
-        module: {
-            rules: [
-                {
-                    exclude: /node_modules/,
-                    test: /\.js$/,
-                    loader: 'babel-loader',
-                    query: {
-                        presets: ['@babel/env', '@babel/react']
-                    }
-                },
-                {
-                    exclude: /node_modules/, // don't transpile node_modules
-                    test: /\.jsx$/, // do transpile any files ending in .jsx
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            plugins: ['transform-react-jsx']
-                        }
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
-                },
-                {
-                    test: /\.less$/,
-                    use: [
-                        {
-                            loader: 'style-loader' // creates style nodes from JS strings
-                        },
-                        {
-                            loader: 'css-loader' // translates CSS into CommonJS
-                        },
-                        {
-                            loader: 'less-loader' // compiles Less to CSS
-                        }
-                    ]
-                },
-                { test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/, loader: 'url-loader?limit=100000' }
-            ]
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          loader: "url-loader",
+          options: {
+            limit: 1,
+            name: "fonts/[name].[ext]"
+          }
         }
-        // devtool: 'eval-sourcemap'
-    };
-
-    return conf;
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([{ from: "src/images", to: "images" }]),
+      new MiniCssExtractPlugin({
+        filename:
+          "styles/" + (devMode ? "[name].css" : "[name].[contenthash:7].css"),
+        chunkFilename:
+          "styles/" + (devMode ? "[name].css" : "[name].[contenthash:7].css")
+      }),
+      new webpack.DefinePlugin({
+        APP_TITLE: JSON.stringify(APP_TITLE),
+        BUILD_DATETIME: JSON.stringify(BUILD_DATETIME),
+        "process.env": {
+          NODE_ENV: JSON.stringify(NODE_ENV)
+        }
+      }),
+      new HtmlWebpackPlugin({
+        chunks: ["manifest", "vendors", "app"],
+        title: APP_TITLE,
+        minify: false,
+        filename: path.resolve(__dirname, "public/index.html"),
+        template: path.resolve(__dirname, "src/index.ejs"),
+        inject: "body",
+        chunksSortMode: "manual",
+        xhtml: true
+      }),
+      new webpack.HashedModuleIdsPlugin({
+        hashDigestLength: 7
+      }),
+      ...plugins
+    ],
+    devtool: devMode && "eval",
+    stats: {
+      children: false,
+      colors: true
+    },
+    devServer: {
+      port: 8080,
+      contentBase: path.resolve(__dirname, "public"),
+      publicPath: "/",
+      open: true,
+      overlay: true,
+      historyApiFallback: true
+    }
+  };
 };
